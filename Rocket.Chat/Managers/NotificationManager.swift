9eba1760c12b4ade0c4767b10976010c79d8614f
Winston Zhang <winston.zhangwentong@gmail.com>
2019-01-10 11:54:52 +0800

    推送测试

--- Rocket.Chat/Managers/NotificationManager.swift
+++ Rocket.Chat/Managers/NotificationManager.swift
@@ -7,8 +7,11 @@
 //
 
 import Foundation
+import UserNotifications
 
-final class NotificationManager {
+
+final class NotificationManager{
+    
     static let shared = NotificationManager()
 
     var notification: ChatNotification?
@@ -28,7 +31,77 @@ final class NotificationManager {
         DispatchQueue.main.async {
             post(notification: notification)
         }
+        
+        ///send the localNotification
+//        takeNotification(notification: notification)
+        
     }
+    
+    
+    ///the method is added by steve
+    static func takeNotification(notification : ChatNotification){
+        
+        ///1.设置通知内容
+        let content = UNMutableNotificationContent();
+        content.title = notification.title
+//        content.subtitle = "点击查看"
+        content.body = notification.body
+        content.categoryIdentifier = notification.payload.id
+        content.userInfo = convertJsonToAnyHashable(notification: notification)
+        
+        ///2.设置触发器
+        let triger = UNTimeIntervalNotificationTrigger(timeInterval: 0.0001, repeats: false)
+        
+        ///3.通知
+        let request = UNNotificationRequest(identifier: notification.payload.id, content: content, trigger: triger)
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        
+        notificationCenter.add(request) { error in
+            
+            if(error != nil){
+                
+                print("推送失败")
+                
+            }else{
+                
+                print("推送成功")
+            }
+            
+        }
+        
+    }
+    
+    static func convertJsonToAnyHashable(notification : ChatNotification) -> [AnyHashable: Any] {
+        return  [
+            "aps": [
+                "alert": [
+                    "title": notification.title,
+                    "body": notification.body,
+                    "sound": "chime.aiff"
+                ],
+                "badge": 1
+            ],
+            "ejson": "{\"host\":\"https://www.chat-stg.baifu-tech.net/\",\"rid\":\"\(notification.payload.rid)\",\"sender\":{\"_id\":\"\(notification.payload.id)\",\"username\":\"\(notification.payload.sender.username)\",\"name\":\"\(notification.payload.name ?? "")\"},\"type\":\"\(notification.payload.internalType)\",\"name\":\"\(notification.payload.name ?? "")\"}",
+
+//            "ejson": "{\"host\":\"kServers\",\"rid\":\"\(notification.payload.rid)\",\"sender\":{\"_id\":\"\(notification.payload.id)\",\"username\":\"\(notification.payload.sender.username)\",\"name\":\"\(notification.payload.name ?? "")\"},\"type\":\"\(notification.payload.internalType)\",\"name\":\"\(notification.payload.name ?? "")\"}",
+            
+            "messageFrom": "push"
+            ] as [AnyHashable: Any]
+    }
+
+    
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        AnalyticsManager.log(event: .replyNotification)
+        
+        PushManager.handleNotification(
+            raw: response.notification.request.content.userInfo,
+            reply: (response as? UNTextInputNotificationResponse)?.userText
+        )
+    }
+
 
     /// Posts an in-app notification.
     ///
